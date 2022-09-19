f55acdc342a3a75e8439581722463fbf4235dfe2
bare74 <bjornarenielsen@yahoo.no>
2022-09-20 01:13:21 +0200

    added json file ++

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const sessions = require("express-session");
+
+var data = fs.readFileSync("data.json");
+var myObject = JSON.parse(data);
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -23,29 +27,20 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 app.use(cookieParser());
+app.use(express.json());
 
 //username and password
-const myusername = "gokstad";
-const mypassword = "skolen";
+const myusername = "John";
+const mypassword = "London";
 
 let session = [];
-
-// app.get("/", (req, res) => {
-//   session = req.session;
-//   if (session.userid) {
-//     res.send("Welcome User <a href='/logout'>click to logout</a>");
-//   } else res.sendFile("index.html", { root: __dirname });
-// });
 
 app.post("/user", (req, res) => {
   if (req.body.username == myusername && req.body.password == mypassword) {
     res.cookie("username", req.body.username);
     res.cookie("password", req.body.password);
     session = req.session;
-    session.userid = req.body.username;
-    console.log(req.body);
 
-    // res.send(`Hei, velkommen inn <a href=\'/logout'>Klikk her for logg av</a>`);
     res.sendFile("userpage.html", {
       root: __dirname,
     });
@@ -54,11 +49,6 @@ app.post("/user", (req, res) => {
   }
 });
 
-// app.get("/logout", (req, res) => {
-//   req.session.destroy();
-//   res.redirect("/");
-// });
-
 app.get("/userpage", function (req, res) {
   res.sendFile("userpage.html", {
     root: __dirname,
@@ -66,40 +56,28 @@ app.get("/userpage", function (req, res) {
 });
 
 app.post("/userpage", function (req, res) {
-  //   console.log(`Hei ${req.cookies.username}`);
-  //   myusername.replace("dog", "monkey");
-  //   console.log(`Hei ${req.cookies.username}`);
-  //   res.redirect("/userpage");
-  //   req.body.mypassword(req.body.password, (err, user) => {
-  //     if (err) {
-  //       res.send(err);
-  //     } else {
-  //       user.mypassword(
-  //         req.body.oldpassword,
-  //         req.body.newpassword,
-  //         function (err) {
-  //           if (err) {
-  //             res.send(err);
-  //           } else {
-  //             res.send("successfully change password");
-  //           }
-  //         }
-  //       );
-  //     }
-  //   });
-  //   if (req.body.oldpassword == myusername) {
-  req.body.oldpassword.replace(req.body.newpassword);
-  res.cookie("password", req.body.password);
-  session = req.session;
-  session.userid = req.body.username;
-  console.log("Ny linje", req.body.newpassword);
-  //   }
+  if (mypassword == req.body.oldpassword) {
+    req.body.oldpassword.replace(req.body.newpassword);
+    res.cookie("new-password", req.body.newpassword);
+    session = req.session;
+
+    let newData = {
+      newpassword: `${req.body.newpassword}`,
+    };
+
+    myObject.push(newData);
+    var newData2 = JSON.stringify(myObject);
+    fs.writeFile("data2.json", newData2, (err) => {
+      // Error checking
+      if (err) throw err;
+      console.log("New data added");
+      res.send("Gratulere passord er oppdatert!");
+    });
+  } else {
+    res.send("Feil passord");
+  }
 });
 
 app.listen(port, () =>
   console.log(`Server started on port ${port} and http://${hostname}`)
 );
-
-// res.send(
-//   `h<1>Login <button type="button" onclick="location.href = '/'; ">HOME </button>`
-// );
